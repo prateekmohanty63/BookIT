652d0abb9a360d6d33a99ce170e99a41e0e0f4c8
Prateek <prateekmohanty63@gmail.com>
2023-06-11 22:53:01 +0530

    Feat:Room-api tested create || delete rooms

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,9 +1,13 @@
 import express from "express"
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router =express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/',getRooms)
+router.get("/:id",getRoom)
 
-})
-
+router.post("/:hotelid",verifyAdmin,createRoom)
+router.put("/:id",verifyAdmin,updateRoom)
+router.delete("/:id/:hotelid",verifyAdmin,deleteRoom)
 export default router
