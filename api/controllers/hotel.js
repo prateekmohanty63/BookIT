9b9fb689e73f29c4ba6483256c3d2719e0a5d269
Prateek <prateekmohanty63@gmail.com>
2023-06-12 19:27:49 +0530

    Feat:HOTEL-endpoint tested get count of hotels by city

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -51,3 +51,18 @@ export const getHotelById=async(req,res,next)=>{
       next(err)
     }
 }
+
+// get count by cities
+export const countByCity=async (req,res,next)=>{
+    const cities=req.query.cities.split(",")
+
+    try{
+       const list=await Promise.all(cities.map(city=>{
+         return Hotel.countDocuments({city:city})
+       }))
+       res.status(200).json(list)
+    }
+    catch(err){
+        next(err)
+    }
+}
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,6 @@
 import express from "express"
 import Hotel from "../models/Hotel.js"
-import { createHotel,updateHotel,deleteHotel,getAllHotel,getHotelById} from "../controllers/hotel.js"
+import { createHotel,updateHotel,deleteHotel,getAllHotel,getHotelById, countByCity} from "../controllers/hotel.js"
 import { verifyAdmin } from "../utils/verifyToken.js"
 
 const router=express.Router()
@@ -12,12 +12,15 @@ router.post("/",verifyAdmin,createHotel)
 router.put("/:id",verifyAdmin,updateHotel)
 
 // GET BY ID 
-router.get("/:id",getHotelById)
+router.get("/find/:id",getHotelById)
 
 // GET ALL HOTELS 
 router.get("/",getAllHotel)
 
 // DELETE HOTEL BY ID
 router.delete("/:id",verifyAdmin,deleteHotel)
+
+router.get("/countByCity",countByCity);
+router.get("/countByType")
 
 export default router
