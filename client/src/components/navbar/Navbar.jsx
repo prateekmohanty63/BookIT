bb2722678d04f13d5c631765e2175d52a255d61c
Prateek <prateekmohanty63@gmail.com>
2023-06-12 21:59:55 +0530

    Feat:Frontend featured list dynamic

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import "./navbar.css"
+import {Navigate} from "react-router-dom"
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
+        <Navigate to="/" style={{color:"inherit",textDecoration:"none"}}>
         <span className="logo">lamabooking</span>
+        </Navigate>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
